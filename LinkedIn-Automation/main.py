764bf2bbf1701d3f13a5cb71a91fd22c2b1204bb
IsmaPi <ismael.picazo.flores@icloud.com>
2024-02-13 15:20:06 +0100

    Got something that (more or less) works

--- LinkedIn-Automation/main.py
+++ LinkedIn-Automation/main.py
@@ -1,15 +1,27 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from hackernews import HackerNews
+from link_extractor import get_top_story_links
 
-hn = HackerNews()
-top_story_ids = hn.top_stories()
-for story in top_story_ids:
-    print(hn.get_item(story))
+top_story_links = get_top_story_links()
 
-#driver = webdriver.Chrome()
-#driver.get(url)
-#results = []
-#content = driver.page_source
-#soup = BeautifulSoup(content, 'html.parser')
+driver = webdriver.Chrome()
+
+# This function gets all the text in each url
+
+
+def get_all_text():
+
+    data = {'url': [], 'text': []}
+
+    for url in top_story_links:
+        driver.get(url)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        text = soup.get_text()
+        data['url'].append(url)
+        data['text'].append(text)
+
+    return pd.DataFrame(data)
+
+
+print(get_all_text())
