2a26282c48c8aa1595ccaab869d3b2af2443fc1d
IsmaPi <ismael.picazo.flores@icloud.com>
2024-02-13 15:30:29 +0100

    Added debugging/next steps in comments

--- LinkedIn-Automation/main.py
+++ LinkedIn-Automation/main.py
@@ -21,7 +21,17 @@ def get_all_text():
         data['url'].append(url)
         data['text'].append(text)
 
+        print(f'Finished scraping {url}')
+        print(f'Full text: {text}')
+
+        # TODO: Clean the text and remove any unwanted characters
+
+        # TODO: Save the data to a json file to be sent to the NLP model
+
+        # TODO: Decide on the NLP model to use and load it
+
+        # TODO: Integrate a way to see if the data is actually text or just random stuff
     return pd.DataFrame(data)
 
 
-print(get_all_text())
+get_all_text()
